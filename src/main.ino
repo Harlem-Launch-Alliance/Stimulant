36eba7530d6d4ecec398b28dfb53cf1fe5bc37b3
Jonathan Safer <jonathansafer2@gmail.com>
2022-02-05 15:17:26 -0500

    split out apogee detection

--- src/main.ino
+++ src/main.ino
@@ -7,16 +7,12 @@
 #include <SD.h>               // for data storage on the onboard microSD card
 #include "Adafruit_BMP3XX.h"  // BMP388 library
 #include <math.h>             // for IMU angle conversion
-#include <list>               // for apogee detection
-#include <cmath>              // for apogee detection
 
 // Constants:
 #define SEALEVELPRESSURE_HPA (1013.25)
 const int MPU=0x68;                                 //I2C address of the MPU-6050
 const int chipSelect = BUILTIN_SDCARD;
 const int buzzer = 33;
-const int TIMEFRAME = 3; // should detect apogee about TIMEFRAME/3 seconds after apogee
-const int SAMPLERATE = 20; //data points per second
 
 // Objects:
 Adafruit_BMP3XX bmp;
@@ -130,62 +126,6 @@ void getBMP()
   temperature = bmp.temperature;          // Temperature in Celcius
   pressure = bmp.pressure / 100.0;        // Pressure in hPa
   Altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA) - altOffset; // Approximate altitude in meters
-}
-
-
-bool hasLaunched(double accelX,  double accelY, double accelZ){
-  const int dataPoints = 5;
-  static bool hasLaunched = false;
-  if(hasLaunched)
-    return true;
-  static std::list<double> totalAccel;
-  std::list<double>::iterator it;
-  double currentTotal = sqrt(pow(accelX, 2) + pow(accelY, 2) + pow(accelZ, 2));
-  totalAccel.push_front(currentTotal);
-  //cleanup old data
-  if(totalAccel.size() > dataPoints){
-    totalAccel.pop_back();
-  }
-  hasLaunched = true;
-  for(it = totalAccel.begin(); it != totalAccel.end(); it++){
-    if(*it < 3 || totalAccel.size() < dataPoints){
-      hasLaunched = false;
-    }
-  }
-  return hasLaunched;
-}
-
-bool detectApogee(double accelX, double accelY, double accelZ, double altitude){ //accel in Gs
-  if(!hasLaunched(accelX, accelY, accelZ))
-    return false;
-  const int dataPoints = TIMEFRAME * SAMPLERATE;
-  static bool apogeeReached = false;
-  static std::list<double> altitudes;
-  std::list<double>::iterator it;
-
-  if(apogeeReached)
-    return true;
-  
-  altitudes.push_front(altitude);
-  if(altitudes.size() > dataPoints){
-    altitudes.pop_back();
-  }
-  if(altitudes.size() < dataPoints){
-    return false;
-  }
-  double first3rd, last3rd = 0;
-  for(it = altitudes.begin(); std::distance(altitudes.begin(),it) < dataPoints/3; it++){
-    first3rd += *it;
-  }
-  for(it = altitudes.end(); std::distance(it, altitudes.end()) <= dataPoints/3; it--){
-    last3rd += *it;
-  }
-  first3rd = first3rd/ (dataPoints/3);
-  last3rd = last3rd/ (dataPoints/3);
-  if(last3rd > first3rd && sqrt(pow(accelX, 2) + pow(accelY, 2) + pow(accelZ, 2)) < 2){
-    apogeeReached = true;
-  }
-  return apogeeReached;
 }
 
 void transmitData()
