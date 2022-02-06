6ec8a3ea0618857b34bc17837fd534ab6049ab33
das-sumit <sumit.das.f@gmail.com>
2022-02-06 15:19:10 -0500

    Added GPS file and separated IMU code
    Added GPS hardware serial code in its own file. Removed all IMU code from main. Added a separate file for IMU code to be used with main later on.

---
+++ main/GY521_Electrosome/GY521_Electrosome.ino
@@ -0,0 +1,116 @@
+#include <Wire.h> //library allows communication with I2C / TWI devices
+#include <math.h> //library includes mathematical functions
+
+const int MPU=0x68;                                 //I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;                //16-bit integers
+int AcXcal,AcYcal,AcZcal,GyXcal,GyYcal,GyZcal,tcal; //calibration variables
+double tf,tc,pitch,roll,Ax,Ay,Az,Gx,Gy,Gz;
+
+void setupIMU(){
+  //Acceleration data correction
+  AcXcal = -950;
+  AcYcal = -300;
+  AcZcal = 0;
+  //Temperature correction
+  tcal = -1600;
+  //Gyro correction
+  GyXcal = 480; //480
+  GyYcal = 170; //170
+  GyZcal = 210; //210
+  
+  Wire.beginTransmission(MPU);          //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet sec. 9.2)
+  Wire.write(0x6B);                     //Accessing the register 6B - Power Management (Sec. 4.28)
+  Wire.write(0b00000000);               //Setting SLEEP register to 0. (Required; see Note on p. 9)
+  Wire.endTransmission();  
+  
+  Wire.beginTransmission(0b1101000);    //I2C address of the MPU
+  Wire.write(0x1B);                     //Accessing the register 1B - Gyroscope Configuration (Sec. 4.4) 
+  Wire.write(0x00000000);               //Setting the gyro to FS_SEL=0 (+/- 250 deg/s) 
+  Wire.endTransmission(); 
+  
+  Wire.beginTransmission(0b1101000);    //I2C address of the MPU
+  Wire.write(0x1C);                     //Accessing the register 1C - Acccelerometer Configuration (Sec. 4.5) 
+  Wire.write(0b00010000);               //Setting the accel to AFS_SEL=2 (to +/- 8g)
+  Wire.endTransmission();
+}
+
+void getIMU()
+{
+  Wire.beginTransmission(MPU);    //begin transmission to I2C slave device
+  Wire.write(0x3B);               // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);    //restarts transmission to I2C slave device
+  Wire.requestFrom(MPU,14,true);  //request 14 registers in total  
+
+  //read accelerometer data
+  AcX=Wire.read()<<8|Wire.read(); // 0x3B (ACCEL_XOUT_H) 0x3C (ACCEL_XOUT_L)  
+  AcY=Wire.read()<<8|Wire.read(); // 0x3D (ACCEL_YOUT_H) 0x3E (ACCEL_YOUT_L) 
+  AcZ=Wire.read()<<8|Wire.read(); // 0x3F (ACCEL_ZOUT_H) 0x40 (ACCEL_ZOUT_L)
+  Ax = (AcX + AcXcal)/4096.0;     // For AFS_SEL=2 value is 4096
+  Ay = (AcY + AcYcal)/4096.0;
+  Az = (AcZ + AcZcal)/4096.0;
+
+  //read temperature data 
+  Tmp = Wire.read()<<8|Wire.read(); // 0x41 (TEMP_OUT_H) 0x42 (TEMP_OUT_L)
+  double tx = Tmp + tcal;
+  tc = tx/340 + 36.53;        // temperature in degrees C from datasheet
+  tf = (tc * 9/5) + 32;              // temperature in fahrenheit
+
+  //read gyroscope data
+  GyX=Wire.read()<<8|Wire.read(); // 0x43 (GYRO_XOUT_H) 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read(); // 0x45 (GYRO_YOUT_H) 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read(); // 0x47 (GYRO_ZOUT_H) 0x48 (GYRO_ZOUT_L) 
+  Gx = (GyX + GyXcal)/131.0;      // For FS_SEL=0 value is 131
+  Gy = (GyY + GyYcal)/131.0;
+  Gz = (GyZ + GyZcal)/131.0;
+
+  //get pitch/roll
+  getAngle(AcX,AcY,AcZ);
+}
+
+//function to convert accelerometer values into pitch and roll
+void getAngle(int Ax,int Ay,int Az) 
+{
+  double x = Ax;
+  double y = Ay;
+  double z = Az;
+  pitch = atan(x/sqrt((y*y) + (z*z)));
+  roll = atan(y/sqrt((x*x) + (z*z)));
+  //converting radians into degrees
+  pitch = pitch * (180.0/3.14);
+  roll = roll * (180.0/3.14) ;
+}
+
+void printData()
+{
+  Serial.print("Angle: ");
+  Serial.print("Pitch = ");
+  Serial.print(pitch);
+  Serial.print(" Roll = ");
+  Serial.println(roll);
+
+  Serial.print("Accelerometer: ");
+  Serial.print("X = "); Serial.print(Ax);
+  Serial.print(" Y = "); Serial.print(Ay);
+  Serial.print(" Z = "); Serial.println(Az); 
+
+  Serial.print("Temperature in celsius = "); Serial.print(tc);  
+  Serial.print(" fahrenheit = "); Serial.println(tf);  
+  
+  Serial.print("Gyroscope: ");
+  Serial.print("X = "); Serial.print(Gx);
+  Serial.print(" Y = "); Serial.print(Gy);
+  Serial.print(" Z = "); Serial.println(Gz);
+}
+
+void setup()
+{
+  Wire.begin();         //initiate wire library and I2C
+  setupIMU();
+  Serial.begin(9600);   //serial communication at 9600 bauds
+}
+
+void loop()
+{
+  getIMU();
+  printData();
+}
--- main/main.ino
+++ main/main.ino
@@ -6,11 +6,9 @@
 #include <Wire.h>             // to communicate with I2C devices
 #include <SD.h>               // for data storage on the onboard microSD card
 #include "Adafruit_BMP3XX.h"  // BMP388 library
-#include <math.h>             // for IMU angle conversion
 
 // Constants:
 #define SEALEVELPRESSURE_HPA (1013.25)
-const int MPU=0x68;                                 //I2C address of the MPU-6050
 const int chipSelect = BUILTIN_SDCARD;
 const int buzzer = 33;
 
@@ -19,86 +17,9 @@ Adafruit_BMP3XX bmp;
 File dataFile;
 
 // Variables:
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;                      // 16-bit integers to store raw IMU data
-int AcXcal,AcYcal,AcZcal,GyXcal,GyYcal,GyZcal,tcal;       // calibration variables
-double tc,pitch,roll,Ax,Ay,Az,Gx,Gy,Gz;                   // final values
 double Altitude, temperature, pressure, altOffset = 0;    // final values
 float Time;
 bool apogeeReached;
-
-void setupIMU(){
-  Serial1.print("Setting up GY521: ");
-  //Acceleration data correction
-  AcXcal = -950;
-  AcYcal = -300;
-  AcZcal = 0;
-  //Temperature correction
-  tcal = -1600;
-  //Gyro correction
-  GyXcal = 480;
-  GyYcal = 170;
-  GyZcal = 210;
-  
-  Wire.beginTransmission(MPU);          //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet sec. 9.2)
-  Wire.write(0x6B);                     //Accessing the register 6B - Power Management (Sec. 4.28)
-  Wire.write(0b00000000);               //Setting SLEEP register to 0. (Required; see Note on p. 9)
-  Wire.endTransmission();  
-  
-  Wire.beginTransmission(0b1101000);    //I2C address of the MPU
-  Wire.write(0x1B);                     //Accessing the register 1B - Gyroscope Configuration (Sec. 4.4) 
-  Wire.write(0x00000000);               //Setting the gyro to FS_SEL=0 (+/- 250 deg/s) 
-  Wire.endTransmission(); 
-  
-  Wire.beginTransmission(0b1101000);    //I2C address of the MPU
-  Wire.write(0x1C);                     //Accessing the register 1C - Acccelerometer Configuration (Sec. 4.5) 
-  Wire.write(0b00010000);               //Setting the accel to AFS_SEL=2 (to +/- 8g)
-  Wire.endTransmission();
-  Serial1.println("successful");
-}
-
-void getIMU()
-{
-  Wire.beginTransmission(MPU);    //begin transmission to I2C slave device
-  Wire.write(0x3B);               // starting with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);    //restarts transmission to I2C slave device
-  Wire.requestFrom(MPU,14,true);  //request 14 registers in total  
-
-  //read accelerometer data
-  AcX=Wire.read()<<8|Wire.read(); // 0x3B (ACCEL_XOUT_H) 0x3C (ACCEL_XOUT_L)  
-  AcY=Wire.read()<<8|Wire.read(); // 0x3D (ACCEL_YOUT_H) 0x3E (ACCEL_YOUT_L) 
-  AcZ=Wire.read()<<8|Wire.read(); // 0x3F (ACCEL_ZOUT_H) 0x40 (ACCEL_ZOUT_L)
-  Ax = (AcX + AcXcal)/4096.0;     // For AFS_SEL=2 value is 4096
-  Ay = (AcY + AcYcal)/4096.0;
-  Az = (AcZ + AcZcal)/4096.0;
-
-  //read temperature data 
-  Tmp = Wire.read()<<8|Wire.read(); // 0x41 (TEMP_OUT_H) 0x42 (TEMP_OUT_L)
-  double tx = Tmp + tcal;
-  tc = tx/340 + 36.53;              // temperature in degrees C from datasheet
-
-  //read gyroscope data
-  GyX=Wire.read()<<8|Wire.read(); // 0x43 (GYRO_XOUT_H) 0x44 (GYRO_XOUT_L)
-  GyY=Wire.read()<<8|Wire.read(); // 0x45 (GYRO_YOUT_H) 0x46 (GYRO_YOUT_L)
-  GyZ=Wire.read()<<8|Wire.read(); // 0x47 (GYRO_ZOUT_H) 0x48 (GYRO_ZOUT_L) 
-  Gx = (GyX + GyXcal)/131.0;      // For FS_SEL=0 value is 131
-  Gy = (GyY + GyYcal)/131.0;
-  Gz = (GyZ + GyZcal)/131.0;
-
-  //get pitch/roll
-  getAngle(AcX,AcY,AcZ);
-}
-
-//function to convert accelerometer values into pitch and roll
-void getAngle(int Ax,int Ay,int Az) 
-{
-  double x = Ax;
-  double y = Ay;
-  double z = Az;
-  pitch = atan(x/sqrt((y*y) + (z*z)));
-  roll = atan(y/sqrt((x*x) + (z*z)));
-  pitch = pitch * (180.0/3.14);           // converting radians into degrees
-  roll = roll * (180.0/3.14) ;
-}
 
 void setupBMP()
 {
@@ -134,14 +55,14 @@ void transmitData()
   Serial1.print(Time); Serial1.print("\t");
   Serial1.print(tc); Serial1.print("\t");
   Serial1.print(temperature); Serial1.print("\t");
-  Serial1.print(pitch); Serial1.print("\t");
-  Serial1.print(roll); Serial1.print("\t");
-  Serial1.print(Ax); Serial1.print("\t");
-  Serial1.print(Ay); Serial1.print("\t");
-  Serial1.print(Az); Serial1.print("\t");
-  Serial1.print(Gx); Serial1.print("\t");
-  Serial1.print(Gy); Serial1.print("\t");
-  Serial1.print(Gz); Serial1.print("\t");
+//  Serial1.print(pitch); Serial1.print("\t");
+//  Serial1.print(roll); Serial1.print("\t");
+//  Serial1.print(Ax); Serial1.print("\t");
+//  Serial1.print(Ay); Serial1.print("\t");
+//  Serial1.print(Az); Serial1.print("\t");
+//  Serial1.print(Gx); Serial1.print("\t");
+//  Serial1.print(Gy); Serial1.print("\t");
+//  Serial1.print(Gz); Serial1.print("\t");
   Serial1.print(pressure); Serial1.print("\t"); Serial1.print("\t");    
   Serial1.print(Altitude); Serial1.print("\t");
   Serial1.println(apogeeReached);
@@ -153,14 +74,14 @@ void recordData()
     dataFile.print(Time); dataFile.print("\t");
     dataFile.print(tc); dataFile.print("\t");
     dataFile.print(temperature); dataFile.print("\t");
-    dataFile.print(pitch); dataFile.print("\t");
-    dataFile.print(roll); dataFile.print("\t");
-    dataFile.print(Ax); dataFile.print("\t");
-    dataFile.print(Ay); dataFile.print("\t");
-    dataFile.print(Az); dataFile.print("\t");
-    dataFile.print(Gx); dataFile.print("\t");
-    dataFile.print(Gy); dataFile.print("\t");
-    dataFile.print(Gz); dataFile.print("\t");
+//    dataFile.print(pitch); dataFile.print("\t");
+//    dataFile.print(roll); dataFile.print("\t");
+//    dataFile.print(Ax); dataFile.print("\t");
+//    dataFile.print(Ay); dataFile.print("\t");
+//    dataFile.print(Az); dataFile.print("\t");
+//    dataFile.print(Gx); dataFile.print("\t");
+//    dataFile.print(Gy); dataFile.print("\t");
+//    dataFile.print(Gz); dataFile.print("\t");
     dataFile.print(pressure); dataFile.print("\t"); dataFile.print("\t");
     dataFile.print(Altitude); dataFile.print("\t");
     dataFile.println(apogeeReached);
@@ -182,7 +103,6 @@ void setup()
   }
   Wire.begin();               // initiate wire library and I2C
   Serial1.begin(115200);      // xBee 115200, 9600
-  setupIMU();
   setupBMP();
   
   // set up sd card
@@ -215,7 +135,6 @@ void setup()
 void loop()
 {
   Time = float(millis()) / 1000.0;
-  getIMU();
   getBMP();
   apogeeReached = detectApogee(Ax, Ay, Az, Altitude);
   recordData();
