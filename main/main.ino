78783a50b592cadd328eeba22cfa9a42a9b4c2d1
Jonathan Safer <jonathansafer2@gmail.com>
2022-06-28 16:58:03 -0400

    re-enabled buzzer

--- main/main.ino
+++ main/main.ino
@@ -25,14 +25,14 @@ bool apogeeReached;
 
 void setup()
 {
-//  pinMode(buzzer, OUTPUT);    // Set buzzer pin as an output
-//  for(int i = 0; i < 5; i++)  // Play 5 beeps
-//  {
-//    tone(buzzer, 1000);       // Send 1KHz sound signal...
-//    delay(1000);              // ...for 1 sec
-//    noTone(buzzer);           // Stop sound...
-//    delay(1000);              // ...for 1sec
-//  }
+  pinMode(buzzer, OUTPUT);    // Set buzzer pin as an output
+  for(int i = 0; i < 5; i++)  // Play 5 beeps
+  {
+    tone(buzzer, 1000);       // Send 1KHz sound signal...
+    delay(1000);              // ...for 1 sec
+    noTone(buzzer);           // Stop sound...
+    delay(1000);              // ...for 1sec
+  }
   Wire.begin();               // initiate wire library and I2C
   Serial1.begin(115200);      // xBee baudrate: 115200, 9600
   Serial1.print("\n\n\n");
