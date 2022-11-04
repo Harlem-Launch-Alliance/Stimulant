01c26f11ea9343d6ac6a9cdb05950f1c02bc78f6
iDaManall <98788676+iDaManall@users.noreply.github.com>
2022-11-04 08:43:00 -0700

    changed all const to constexpr (#123)
    Update settings.h

--- main/settings.h
+++ main/settings.h
@@ -6,13 +6,13 @@
 #define SETTINGS_H
 
 //tick times (microseconds)
-const uint32_t TICK_TIME_ONPAD = 10000;
-const uint32_t TICK_TIME_ASCENDING = 10000;
-const uint32_t TICK_TIME_DESCENDING = 50000;
-const uint32_t TICK_TIME_POST_FLIGHT = 1000000;
+constexpr uint32_t TICK_TIME_ONPAD = 10000;
+constexpr uint32_t TICK_TIME_ASCENDING = 10000;
+constexpr uint32_t TICK_TIME_DESCENDING = 50000;
+constexpr uint32_t TICK_TIME_POST_FLIGHT = 1000000;
 
-const uint32_t RING_QUEUE_LENGTH = 3000;//each 100 elements is 1 seconds of data at 100hz
+constexpr uint32_t RING_QUEUE_LENGTH = 3000;//each 100 elements is 1 seconds of data at 100hz
 
-const uint32_t GPS_WAIT_TIME = 300; //number of seconds to wait for GPS to acquire signal before moving on
+constexpr uint32_t GPS_WAIT_TIME = 300; //number of seconds to wait for GPS to acquire signal before moving on
 
 #endif
