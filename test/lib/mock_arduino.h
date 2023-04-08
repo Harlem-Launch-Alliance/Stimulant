ddbf255fbfbe2a23bbc09d31fe6b255d9d20ec8b
Jonathan Safer <jonathansafer2@gmail.com>
2023-04-08 12:28:43 -0400

    added unit tests for bmpAltimeter

--- test/lib/mock_arduino.h
+++ test/lib/mock_arduino.h
@@ -1,3 +1,73 @@
 #pragma once
 
+#include <string>
+
 #define PI 3.1415926535897932384626433832795
+#define BMP3_NO_OVERSAMPLING 1
+#define BMP3_OVERSAMPLING_2X 1
+#define BMP3_IIR_FILTER_COEFF_3 1
+#define BMP3_ODR_100_HZ 1
+
+class Adafruit_BMP3XX
+{
+public:
+    bool performReading() {
+        pressure = 100;
+        temperature = 1;
+        return true;
+    }
+
+    double readAltitude(double groundPressure) {
+        (void)groundPressure;
+        return 1;
+    }
+
+    bool begin_SPI(int chipSelect) {
+        (void)chipSelect;
+        return true;
+    }
+
+    void setTemperatureOversampling(int setting) {
+        (void)setting;
+    }
+
+    void setPressureOversampling(int setting) {
+        (void)setting;
+    }
+    
+    void setIIRFilterCoeff(int setting) {
+        (void)setting;
+    }
+
+    void setOutputDataRate(int setting) {
+        (void)setting;
+    }
+
+    double pressure;
+    double temperature;
+
+};
+
+unsigned long micros() {
+    static unsigned long micros = 1;
+    micros += 10;
+    return micros;
+}
+
+class Serial
+{
+public:
+    void print(std::string s) {
+        (void)s;
+    }
+
+    void println(std::string s) {
+        (void)s;
+    }
+};
+
+Serial Serial1;
+
+void delay(int millis) {
+    (void)millis;
+}
---
+++ test/test_bmp_altimeter/test_main.cpp
@@ -0,0 +1,49 @@
+#include <unity.h>
+#include "../main/bmp_altimeter.h"
+
+void setUp(void)
+{
+    // set stuff up here
+    GNDLEVELPRESSURE_HPA = 0;
+}
+
+void tearDown(void)
+{
+    // clean stuff up here
+}
+
+/// @brief test pressure calibration
+/// @param void
+void test_set_ground_level_pressure(void)
+{
+    setGroundLevelPressure();
+    TEST_ASSERT_EQUAL(1, GNDLEVELPRESSURE_HPA);
+}
+
+/// @brief ensure this function runs without errors
+/// @param void
+void test_setup_bmp(void)
+{
+    setupBMP();
+    TEST_ASSERT_EQUAL(1, GNDLEVELPRESSURE_HPA);
+}
+
+/// @brief verify info from getBMP
+/// @param void
+void test_get_bmp(void)
+{
+    bmpReading sample = getBMP();
+    TEST_ASSERT_EQUAL(1, sample.altitude);
+    TEST_ASSERT_TRUE(sample.time > 0);
+}
+
+int main()
+{
+    UNITY_BEGIN(); // IMPORTANT LINE!
+
+    RUN_TEST(test_set_ground_level_pressure);
+    RUN_TEST(test_setup_bmp);
+    RUN_TEST(test_get_bmp);
+
+    UNITY_END(); // stop unit testing
+}
